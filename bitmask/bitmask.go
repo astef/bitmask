d5e72d67074f21ebc3de98f042000b4ed2092cae
Alexander Stefurishin <a.stefurishin@gmail.com>
2023-03-20 00:55:00 +0300

    slice operation

--- bitmask/bitmask.go
+++ bitmask/bitmask.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 )
 
-const uintSize = 32 << (^uint(0) >> 63) // 32 or 64
+const uintMax = ^uint(0)
+const uintSize = 32 << (uintMax >> 63) // 32 or 64
+
+var uintFormat = fmt.Sprintf("%%0%vb", uintSize)
+
+const maxStringedUints = 8
 
 type BitMask struct {
 	store []uint
 	len   uint
+	// always in range [0 ; uintSize)
+	shift uint
 }
 
 func New(len uint) *BitMask {
@@ -21,13 +28,14 @@ func NewFromUint(values ...uint) *BitMask {
 	return &BitMask{store: store, len: uintSize * uint(len(values))}
 }
 
+// Returns the legth of bitmask in bits. It will never be changed for the given receiver.
 func (bm *BitMask) Len() uint {
 	return bm.len
 }
 
 func (bm *BitMask) Set(index uint) {
 	checkBounds(bm.len, index)
-	bref, m := findBit(bm.store, index)
+	bref, m := bm.getBit(index)
 	*bref |= m
 }
 
@@ -41,75 +49,223 @@ func (bm *BitMask) Set(index uint) {
 
 func (bm *BitMask) Clear(index uint) {
 	checkBounds(bm.len, index)
-	bref, m := findBit(bm.store, index)
+	bref, m := bm.getBit(index)
 	*bref &^= m
 }
 
 func (bm *BitMask) Toggle(index uint) {
 	checkBounds(bm.len, index)
-	bref, m := findBit(bm.store, index)
+	bref, m := bm.getBit(index)
 	*bref ^= m
 }
 
 func (bm *BitMask) IsSet(index uint) bool {
 	checkBounds(bm.len, index)
-	bref, m := findBit(bm.store, index)
+	bref, m := bm.getBit(index)
 	return (*bref & m) != 0
 }
 
 // Copies bits from a source bit mask into a destination bit mask.
 // Returns the number of bits copied, which will be the minimum of src.Len() and dst.Len().
 func Copy(dst *BitMask, src *BitMask) uint {
-	bitsN := dst.len
-	if src.len < dst.len {
-		bitsN = src.len
+	copyLen := minUint(src.len, dst.len)
+	if copyLen == 0 {
+		return 0
 	}
 
-	// copy whole part
-	wordsN := bitsN / uintSize
-	copy(dst.store[:wordsN], src.store[:wordsN])
+	srcShift := src.shift
+	dstShift := dst.shift
 
-	// copy remainder
-	remainderBitsN := bitsN % uintSize
-	if remainderBitsN == 0 {
-		return bitsN
+	if srcShift == dstShift {
+		var copyStartIndex uint = 0
+		copyEndIndex := ((copyLen - 1 + srcShift) / uintSize) + 1
+		remainderBitsN := (copyLen + srcShift) % uintSize
+
+		if srcShift != 0 {
+			// have to copy first uint manually
+			copyUintPart(
+				uintSize-srcShift,
+				src.store[copyStartIndex],
+				srcShift,
+				&dst.store[copyStartIndex],
+				dstShift,
+			)
+			copyStartIndex++
+		}
+
+		if remainderBitsN != 0 {
+			// have to copy last uint manually
+			copyEndIndex--
+			copyUintPart(
+				remainderBitsN,
+				src.store[copyEndIndex],
+				0,
+				&dst.store[copyEndIndex],
+				0,
+			)
+		}
+
+		if copyStartIndex != copyEndIndex {
+			// copy whole part
+			copy(dst.store[copyStartIndex:copyEndIndex], src.store[copyStartIndex:copyEndIndex])
+		}
+	} else {
+		// not optimized copy, by uint parts
+		availableInSrc := src.len
+		availableInDst := dst.len
+		currentSrcShift := srcShift
+		currentDstShift := dstShift
+		currentSrcIndex := 0
+		currentDstIndex := 0
+		for availableInSrc != 0 && availableInDst != 0 {
+			srcRemainder := minUint(availableInSrc, uintSize-currentSrcShift)
+			dstRemainder := minUint(availableInDst, uintSize-currentDstShift)
+			copyLen = minUint(srcRemainder, dstRemainder)
+
+			copyUintPart(
+				copyLen,
+				src.store[currentSrcIndex],
+				currentSrcShift,
+				&dst.store[currentDstIndex],
+				currentDstShift,
+			)
+
+			availableInSrc -= copyLen
+			availableInDst -= copyLen
+			currentSrcShift += copyLen
+			currentDstShift += copyLen
+
+			if currentSrcShift == uintSize {
+				currentSrcShift = 0
+				currentSrcIndex++
+			} else { // optimizing, using the fact of "src.shift != dst.shift" here
+				currentDstShift = 0
+				currentDstIndex++
+			}
+		}
 	}
 
-	shift := uintSize - remainderBitsN
-	mask := (^uint(0) >> shift) << shift
-	dst.store[wordsN] |= src.store[wordsN] | mask
-
-	return bitsN
+	return copyLen
 }
 
-// // Selects a half-open range which includes the "from" bit, but excludes the "to" one.
-// func (bm BitMask) Slice(fromIndex uint64, toIndex uint64) BitMask {
-// 	fromWordIndex := fromIndex / STORE_WORD_SIZE
+// Selects a half-open range which includes the "from" bit, but excludes the "to" one.
+func (bm *BitMask) Slice(fromBit uint, toBit uint) *BitMask {
+	checkSliceBounds(fromBit, toBit, bm.len)
+	if fromBit == toBit {
+		// to avoid empty BitMask with shift
+		return New(0)
+	}
 
-// 	return BitMask{
-// 		store: bm.store[fromIndex:toIndex],
-// 		len:   toIndex - fromIndex,
+	fromStoreIndex := bm.getStoreIndex(fromBit)
+	toStoreIndex := bm.getStoreIndex(toBit) + 1
+
+	return &BitMask{
+		store: bm.store[fromStoreIndex:toStoreIndex],
+		shift: (fromBit + bm.shift) % uintSize,
+		len:   toBit - fromBit,
+	}
+}
+
+// Returns a closure function, which may be called many times to iterate
+// through all bits and get their value as a bool.
+// Closure will start returning nil after reaching the end.
+func (bm *BitMask) Iterator() func() *bool {
+	index := uint(0)
+	return func() *bool {
+		if index == bm.len {
+			return nil
+		}
+		bref, m := bm.getBit(index)
+		index++
+		isSet := (*bref & m) != 0
+		return &isSet
+	}
+}
+
+// func (bm *BitMask) String() string {
+// 	if bm.len == 0 {
+// 		return "[0]{}"
 // 	}
+// 	var b strings.Builder
+// 	b.WriteString(fmt.Sprintf("[%v]{", bm.len))
+
+// 	// first word
+// 	first := fmt.Sprintf(uintFormat, bm.store[0])[bm.shift:]
+
+// 	// last word
+// 	last := fmt.Sprintf(uintFormat, )
+
+// 	storeLen := uint(len(bm.store))
+// 	end := minUint(storeLen, maxStringedUints)
+
+// 	for n, word := range bm.store[:end] {
+// 		if n != 0 {
+// 			b.WriteString(" ")
+// 		}
+// 		b.WriteString(fmt.Sprintf(uintFormat, word))
+// 	}
+
+// 	skipped := storeLen - end
+// 	if skipped > 0 {
+// 		b.WriteString(fmt.Sprintf(" ...(more %v values)", skipped))
+// 	}
+
+// 	b.WriteString("}")
+// 	return b.String()
 // }
 
-// func (bm BitMask) String() string {
-// 	if bm.len <= 128 {
-// 		return fmt.Sprintf("%08b", bm.store)
-// 	}
-// 	left := bm.store[:64]
-// 	right := bm.store[bm.len-64:]
-// 	return fmt.Sprintf("%08b <skipped  items> %08b", bm.sli)
+func (bm *BitMask) getRemainderLen(bitIndex uint) uint {
+	return (bm.shift + bitIndex) % uintSize
+}
 
-// }
+func (bm *BitMask) getStoreIndex(bitIndex uint) uint {
+	return (bm.shift + bitIndex) / uintSize
+}
+
+func (bm *BitMask) getBit(bitIndex uint) (*uint, uint) {
+	storeIndex := bm.getStoreIndex(bitIndex)
+	remainderLen := bm.getRemainderLen(bitIndex)
+	mask := uint(1) << remainderLen
+	return &bm.store[storeIndex], mask
+}
+
+func minUint(a uint, b uint) uint {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func maxUint(a uint, b uint) uint {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func copyUintPart(len uint, src uint, srcShift uint, dst *uint, dstShift uint) {
+	m := (uintMax << (uintSize - len))
+	srcMask := m >> srcShift
+	tmp := (src & srcMask)
+	if dstShift > srcShift {
+		tmp = tmp >> (dstShift - srcShift)
+	} else {
+		tmp = tmp << (srcShift - dstShift)
+	}
+	*dst |= tmp
+}
+
+func checkSliceBounds(fromIndex uint, toIndex uint, capacity uint) {
+	if fromIndex > toIndex {
+		panic(fmt.Sprintf("slice bounds out of range [%v:%v]", fromIndex, toIndex))
+	}
+	if toIndex > capacity {
+		panic(fmt.Sprintf("slice bounds out of range [:%v] with capacity %v", toIndex, capacity))
+	}
+}
 
 func checkBounds(len uint, index uint) {
 	if index >= len {
-		panic(fmt.Sprintf("index out of range (len=%v, index=%v)", len, index))
+		panic(fmt.Sprintf("index out of range [%v] with length %v", index, len))
 	}
-}
-
-func findBit(store []uint, index uint) (*uint, uint) {
-	storeIndex := index / uintSize
-	byteMask := uint(1) << (index % uintSize)
-	return &store[storeIndex], byteMask
 }
--- bitmask/bitmask_test.go
+++ bitmask/bitmask_test.go
@@ -1,7 +1,6 @@
 package bitmask
 
 import (
-	"math"
 	"math/rand"
 	"testing"
 
@@ -25,7 +24,7 @@ type sctTestCase struct {
 func TestSetClearToggle(t *testing.T) {
 	nS, nM, nL, nXL := genN()
 	each := uint(1)
-	none := uint(math.MaxUint)
+	none := uintMax
 
 	tests := map[string]sctTestCase{
 		"only_set_S":  {nS, each, none, none},
@@ -130,28 +129,27 @@ type copyTestCase struct {
 }
 
 func TestCopy(t *testing.T) {
-
 	tests := map[string]copyTestCase{
 		"equal_size": {
 			src:         []uint{0, 0},
-			srcLen:      128,
-			dst:         []uint{math.MaxUint, math.MaxUint},
+			srcLen:      uintSize * 2,
+			dst:         []uint{uintMax, uintMax},
 			expectedDst: []uint{0, 0},
-			expectedN:   uint(128),
+			expectedN:   uintSize * 2,
 		},
 		"small_src": {
-			src:         []uint{math.MaxUint, 1 << 63},
-			srcLen:      65,
-			dst:         []uint{0, math.MaxUint >> 1, math.MaxUint},
-			expectedDst: []uint{math.MaxUint, math.MaxUint, math.MaxUint},
-			expectedN:   uint(65),
+			src:         []uint{uintMax, 1 << (uintSize - 1)},
+			srcLen:      uintSize + 1,
+			dst:         []uint{0, uintMax >> 1, uintMax},
+			expectedDst: []uint{uintMax, uintMax, uintMax},
+			expectedN:   uintSize + 1,
 		},
 		"small_dst": {
-			src:         []uint{math.MaxUint, math.MaxUint, math.MaxUint},
-			srcLen:      192,
+			src:         []uint{uintMax, uintMax, uintMax},
+			srcLen:      uintSize * 3,
 			dst:         []uint{0, 0},
-			expectedDst: []uint{math.MaxUint, math.MaxUint},
-			expectedN:   uint(128),
+			expectedDst: []uint{uintMax, uintMax},
+			expectedN:   uintSize * 2,
 		},
 	}
 	for name, tc := range tests {
@@ -169,6 +167,106 @@ func TestCopy(t *testing.T) {
 			// assert
 			assert.Equal(t, tc.expectedN, n)
 			assert.Equal(t, expected, dst)
+		})
+	}
+}
+
+type sliceTestCase struct {
+	source BitMask
+
+	from uint
+	to   uint
+
+	expected BitMask
+}
+
+func TestSlice(t *testing.T) {
+	store := []uint{0, uintMax, 0}
+	storeLen := uint(len(store))
+	bm := BitMask{
+		store: store,
+		len:   storeLen * uintSize,
+		shift: 0,
+	}
+	minShiftBm := BitMask{
+		store: store,
+		len:   storeLen*uintSize - 1,
+		shift: 1,
+	}
+	min2ShiftBm := BitMask{
+		store: store,
+		len:   storeLen*uintSize - 2,
+		shift: 2,
+	}
+	maxShiftBm := BitMask{
+		store: store,
+		len:   (storeLen-1)*uintSize + 1,
+		shift: uintSize - 1,
+	}
+	withoutFirstBm := BitMask{
+		store: store[1:],
+		len:   (storeLen - 1) * uintSize,
+		shift: 0,
+	}
+	twoBitsBetweenFirstAndSecondBm := BitMask{
+		store: store[:2],
+		len:   2,
+		shift: uintSize - 1,
+	}
+	tests := map[string]sliceTestCase{
+		"full_no_shift": {
+			source:   bm,
+			from:     0,
+			to:       bm.len,
+			expected: bm,
+		},
+		"full_min_shift": {
+			source:   minShiftBm,
+			from:     0,
+			to:       minShiftBm.len,
+			expected: minShiftBm,
+		},
+		"full_max_shift": {
+			source:   maxShiftBm,
+			from:     0,
+			to:       maxShiftBm.len,
+			expected: maxShiftBm,
+		},
+		"from1_no_shift": {
+			source:   bm,
+			from:     1,
+			to:       bm.len,
+			expected: minShiftBm,
+		},
+		"from1_min_shift": {
+			source:   minShiftBm,
+			from:     1,
+			to:       minShiftBm.len,
+			expected: min2ShiftBm,
+		},
+		"from1_max_shift": {
+			source:   maxShiftBm,
+			from:     1,
+			to:       maxShiftBm.len,
+			expected: withoutFirstBm,
+		},
+		"from0_to2_maxshift": {
+			source:   maxShiftBm,
+			from:     0,
+			to:       2,
+			expected: twoBitsBetweenFirstAndSecondBm,
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			if name != "from0_to2_maxshift" {
+				return
+			}
+
+			actual := tc.source.Slice(tc.from, tc.to)
+
+			// assert
+			assert.Equal(t, tc.expected, *actual)
 		})
 	}
 
